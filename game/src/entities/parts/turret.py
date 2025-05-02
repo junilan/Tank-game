d358811600344934c0cf635514713e6e723f9305
Junil An <black_ahn@hotmail.com>
2025-05-02 02:22:52 +0200

    cleaning up and separating class logic for player, tank body, and turret.

--- game/src/entities/parts/turret.py
+++ game/src/entities/parts/turret.py
@@ -15,15 +15,35 @@ class Turret:
         self.rotaion_speed = playerStatus["rotation_speed"]
         self.direction = pygame.math.Vector2(playerStatus["init_direction"])
 
-    def update(self, dt):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            self.rotate(+self.rotaion_speed)
-        if keys[pygame.K_d]:
-            self.rotate(-self.rotaion_speed)
-       
-    def draw(self, screen):
-        pass
+    def update(self, x, y):
+        self.x = x
+        self.y = y
+        current_tank_body_pos = pygame.Vector2(self.x, self.y)
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        mouse_pos_vector = pygame.Vector2(mouse_x, mouse_y)
+        turret_direction = mouse_pos_vector - current_tank_body_pos
 
-    def rotate(self, angle_degrees):
-        self.direction = self.direction.rotate(angle_degrees)
+        offset_cannonBody = pygame.Vector2(0, -10)  # Offset position to relocate cannon body's pivot point
+
+        if turret_direction.length() > 0: turret_direction = turret_direction.normalize()
+
+        turret_angle = turret_direction.angle_to(pygame.Vector2(0, 1)) - 180.0  # Angle in degrees
+        current_center_turret_pos = current_tank_body_pos
+
+        self.rotated_turretBody_image = pygame.transform.rotate(self.image_files["turret_body"], turret_angle)
+        self.rotated_cannonCover_image = pygame.transform.rotate(self.image_files["cannon_cover"], turret_angle)
+        self.rotated_cannonBody_image = pygame.transform.rotate(self.image_files["cannon_body"], turret_angle)
+        rotated_offset_cannonBody = offset_cannonBody.rotate(-turret_angle)# Offset rotating for cannon body's pivot point
+        
+        self.rotated_rect_turretBody = self.rotated_turretBody_image.get_rect(center = current_center_turret_pos)
+        self.rotated_rect_cannonCover = self.rotated_cannonCover_image.get_rect(center = current_center_turret_pos)
+        self.rotated_rect_cannonBody = self.rotated_cannonBody_image.get_rect(center = current_center_turret_pos + rotated_offset_cannonBody)
+
+    def draw(self, screen):
+        screen.blit(self.rotated_turretBody_image, self.rotated_rect_turretBody)
+        screen.blit(self.rotated_cannonBody_image, self.rotated_rect_cannonBody)
+        screen.blit(self.rotated_cannonCover_image, self.rotated_rect_cannonCover)
+
+    def fire(self):
+        # Implement firing logic here
+        pass
--- game/src/entities/player.py
+++ game/src/entities/player.py
@@ -8,18 +8,45 @@ from src.utils import load_image
 
 class Player:
     def __init__(self, x, y):
-        #self.tank_body_image_file = load_image(config.IMAGEFILE_PATH + "tankBody.png")
-        turret_image_files = {
+        tank_body_image_file = {
             "tank_body": load_image(config.IMAGEFILE_PATH + "tankBody.png"),
+        }
+        turret_image_files = {
             "turret_body": load_image(config.IMAGEFILE_PATH + "turretBody.png"),
             "cannon_cover": load_image(config.IMAGEFILE_PATH + "cannonCover.png"),
             "cannon_body": load_image(config.IMAGEFILE_PATH + "cannon.png"),
         }
+        
+        self.tank_body = TankBody(x, y, tank_body_image_file)
+        self.turret = Turret(x, y, turret_image_files)
 
-        self.tank_body = TankBody(x, y, turret_image_files)
+        self.rotaion_speed = playerStatus["rotation_speed"]
+        self.direction = pygame.math.Vector2(playerStatus["init_direction"])
+        self.speed = 0
+        self.tank_speed = playerStatus["speed"]
 
     def update(self, dt):
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]: # Rotate the tank left
+            self.tank_body.rotate(+self.rotaion_speed)
+        if keys[pygame.K_d]: # Rotate the tank right
+            self.tank_body.rotate(-self.rotaion_speed)
+
+        if keys[pygame.K_w]: # Move the tank forward
+            self.tank_body.move(self.tank_speed)
+        elif keys[pygame.K_s]: # Move the tank backward
+            self.tank_body.move(-self.tank_speed)
+        else: # Stop the tank
+            self.tank_body.move(0)
+            
         self.tank_body.update(dt)
+
+        # Update turret position
+        self.turret.update(self.tank_body.x, self.tank_body.y)
+
 
     def draw(self, screen):
         self.tank_body.draw(screen)
+        self.turret.draw(screen)
